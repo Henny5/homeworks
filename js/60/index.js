d0776257f30657e9fb87800ebdb21fd16b2f7430
Henny Davidowitz <hhalpert18@gmail.com>
2021-07-14 16:54:56 +0300

    homework60final

---
+++ js/60/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var name = 'Henny Davidowitz';/*jshint -W117*///jshint ignore:line
+
+var email= 'hhalpert18@gmail.com';/*jshint -W117*///jshint ignore:line
+
+
+
---
+++ js/60/index2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/*globals name, email*/ 
+
+var age= prompt("how old are you?");
+console.log(  email, name, + age);
+
