3c23209c03c927f4d721742aafb47cedf72d3b72
Henny Davidowitz <hhalpert18@gmail.com>
2021-12-12 13:57:08 +0200

    homework78final

--- js/78/constructor.js
+++ js/78/constructor.js
@@ -1,43 +1,35 @@
+"use strict";
 
-'use strict';
-
-function Vehicle(color,speed){
-    this.color=color;
-    this.speed=speed;
+function Vehicle(color, speed) {
+  this.color = color;
+  this.speed = speed;
 }
 
 Vehicle.prototype.go = function () {
-    console.log(`now going at speed ${this.speed} `);
-  };
+  console.log(`now going at speed ${this.speed} `);
+};
 
- Vehicle.prototype.print = function () {
-    console.log(`${this.color}, ${this.speed} `);
-  };
+Vehicle.prototype.print = function () {
+  console.log(`${this.color}, ${this.speed} `);
+};
 
-const car = new Vehicle('red','50');
+const car = new Vehicle("red", "50");
 console.log(car);
 car.print();
 car.go();
 
-function Plane(color,speed){
-    Vehicle.call(this,color, speed);
-    
+function Plane(color, speed) {
+  Vehicle.call(this, color, speed);
 }
-
 
 Plane.prototype = Object.create(Vehicle.prototype);
 Plane.prototype.constructor = Plane;
 
- Plane.prototype.go = function () {
-    console.log(`now flying at speed ${this.speed} `);
-  };
+Plane.prototype.go = function () {
+  console.log(`now flying at speed ${this.speed} `);
+};
 
-
-const plane = new Plane('blue','700');
+const plane = new Plane("blue", "700");
 console.log(plane);
 plane.print();
 plane.go();
-
-
-
-
